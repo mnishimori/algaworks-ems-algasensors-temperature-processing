4961f751d1e89b6aa73e13a158533f6f161206a8
Marcelo Akio Nishimori <mnishimori@yahoo.com.br>
2026-02-21 19:52:23 -0300

    fix: add missing Spring Boot main class

---
+++ src/main/java/com/algaworks/algasensors/temperature/processing/TemperatureProcessingApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.algasensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureProcessingApplication.class, args);
+    }
+}
